b5eb82a6563f46a397498e4b9aff70e9839a977d
jupiter229 <68340397+jupiter229@users.noreply.github.com>
2021-12-09 07:40:25 -0800

    remove test files

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,26 +1,5 @@
-const fs = require('fs');
+const Migrations = artifacts.require("Migrations");
 
-const TestContract = artifacts.require("TestContract");
-const MockToken = artifacts.require("ERC20Mock");
-
-module.exports = async function (deployer, network, addresses) {
-    const [admin, _] = addresses;
-
-    let lpTokenAddress, offeredTokenAddress;
-    if (network == "testnet") {
-      // deploy the mock tokens here to testnet as they don't exist yet
-      await deployer.deploy(MockToken, "LP Test Token", "LPTT", 1000000);
-    
-      lpTokenAddress = MockToken.address;
-
-      if (!lpTokenAddress) {
-          console.log(`Deployment failed, no lp/offered token address`)
-          return;
-      }
-
-      // Deploy contract
-      await deployer.deploy(
-        TestContract
-      );
-    }
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
 };
--- migrations/5_deploy_breed.js
+++ migrations/5_deploy_breed.js
@@ -2,5 +2,6 @@ const MasterBreeder = artifacts.require('MasterBreeder');
 const GovernanceToken = artifacts.require("../build/contracts/UniswapV2ERC20");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(MasterBreeder);
+   const Gov = await artifacts.require("<gov build>").deployed(); 
+   await deployer.deploy(MasterBreeder, Gov.address, other params, comma, sepparated);
 }
